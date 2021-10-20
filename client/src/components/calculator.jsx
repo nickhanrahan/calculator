fe4e0d643732589f3c1203f5d7c3dc7925711383
nickhanrahan <nicholashanr@gmail.com>
2021-10-19 17:45:40 -0700

    handle button presses to add to expression

--- client/src/components/calculator.jsx
+++ client/src/components/calculator.jsx
@@ -6,14 +6,25 @@ const operators = {}
 
 const Calculator = () => {
   const [expression, setExpression] = useState('');
+  const [curIndex, setCurIndex] = useState(0);
   const [opsList, setOpsList] = useState([]);
   const [parens, setParens] = useState([]);
+
+  const handleButtonPress = (event) => {
+    const newExpression = expression + event.target.value;
+    const newCurIndex = curIndex + 1;
+    setExpression(newExpression);
+    setCurIndex(newCurIndex);
+  }
 
   return (
     <div className="calculator">
       <History />
-      <div className="expression" >{expression}</div>
-      <Keypad />
+      <div className="expression" >
+        <div className="left-block"></div>
+        <div className="exp-text">{expression}</div>
+      </div>
+      <Keypad handleButtonPress={handleButtonPress}/>
     </div>
   )
 }
--- client/src/components/keypad.jsx
+++ client/src/components/keypad.jsx
@@ -1,36 +1,37 @@
 import React from 'react';
 import './style.css';
 
-const Keypad = () => {
+const Keypad = ({ handleButtonPress }) => {
+
   return (
     <div className="keypad">
       <div className="keyrow">
-        <div className="keycol-1-5"><button className="open-p-btn">(</button></div>
-        <div className="keycol-2-5"><button className="seven-btn">7</button></div>
-        <div className="keycol-3-5"><button className="eight-btn">8</button></div>
-        <div className="keycol-4-5"><button className="nine-btn">9</button></div>
-        <div className="keycol-5-5"><button className="divide-btn">&divide;</button></div>
+        <div className="keycol-1-5"><button value="(" className="open-p-btn" onClick={handleButtonPress}>(</button></div>
+        <div className="keycol-2-5"><button value="7" className="seven-btn" onClick={handleButtonPress}>7</button></div>
+        <div className="keycol-3-5"><button value="8" className="eight-btn" onClick={handleButtonPress}>8</button></div>
+        <div className="keycol-4-5"><button value="9" className="nine-btn" onClick={handleButtonPress}>9</button></div>
+        <div className="keycol-5-5"><button value="/" className="divide-btn" onClick={handleButtonPress}>&divide;</button></div>
       </div>
       <div className="keyrow">
-        <div className="keycol-1-5"><button className="close-p-btn">)</button></div>
-        <div className="keycol-2-5"><button className="four-btn">4</button></div>
-        <div className="keycol-3-5"><button className="five-btn">5</button></div>
-        <div className="keycol-4-5"><button className="six-btn">6</button></div>
-        <div className="keycol-5-5"><button className="times-btn">&times;</button></div>
+        <div className="keycol-1-5"><button value=")" className="close-p-btn" onClick={handleButtonPress}>)</button></div>
+        <div className="keycol-2-5"><button value="4" className="four-btn" onClick={handleButtonPress}>4</button></div>
+        <div className="keycol-3-5"><button value="5" className="five-btn" onClick={handleButtonPress}>5</button></div>
+        <div className="keycol-4-5"><button value="6" className="six-btn" onClick={handleButtonPress}>6</button></div>
+        <div className="keycol-5-5"><button value="*" className="times-btn" onClick={handleButtonPress}>&times;</button></div>
       </div>
       <div className="keyrow">
-        <div className="keycol-1-5"><button className="clear-btn">C</button></div>
-        <div className="keycol-2-5"><button className="one-btn">1</button></div>
-        <div className="keycol-3-5"><button className="two-btn">2</button></div>
-        <div className="keycol-4-5"><button className="three-btn">3</button></div>
-        <div className="keycol-5-5"><button className="minus-btn">-</button></div>
+        <div className="keycol-1-5"><button value="C" className="clear-btn" >C</button></div>
+        <div className="keycol-2-5"><button value="1" className="one-btn" onClick={handleButtonPress}>1</button></div>
+        <div className="keycol-3-5"><button value="2" className="two-btn" onClick={handleButtonPress}>2</button></div>
+        <div className="keycol-4-5"><button value="3" className="three-btn" onClick={handleButtonPress}>3</button></div>
+        <div className="keycol-5-5"><button value="-" className="minus-btn" onClick={handleButtonPress}>-</button></div>
       </div>
       <div className="keyrow">
-        <div className="keycol-1-5"><button className="idk-btn"></button></div>
-        <div className="keycol-2-5"><button className="zero-btn">0</button></div>
-        <div className="keycol-3-5"><button className="point-btn">.</button></div>
-        <div className="keycol-4-5"><button className="equal-btn">=</button></div>
-        <div className="keycol-5-5"><button className="plus-btn">+</button></div>
+        <div className="keycol-1-5"><button value="" className="idk-btn" ></button></div>
+        <div className="keycol-2-5"><button value="0" className="zero-btn" onClick={handleButtonPress}>0</button></div>
+        <div className="keycol-3-5"><button value="." className="point-btn" onClick={handleButtonPress}>.</button></div>
+        <div className="keycol-4-5"><button value="=" className="equal-btn" >=</button></div>
+        <div className="keycol-5-5"><button value="+" className="plus-btn" onClick={handleButtonPress}>+</button></div>
       </div>
     </div>
   )
